4e71281f143091dac86469b87eda3e8ac187daac
Intern <intern@jbl.local>
2025-01-14 11:16:33 +0530

    simple deep-linking that uses youtuve

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // Method channel
+  static const platform = MethodChannel('launchUrl');
+
+  // Method to open URL
+  static Future<void> openUrl(String url) async {
+    try {
+      await platform.invokeMethod('openUrl', url).then((v) {
+        print(">>> url has been launched");
+      });
+    } on PlatformException catch (e) {
+      print(">>>Failed to open URL: '${e.message}'.");
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text("Platform-Specific URL Launching")),
+        body: Center(
+          child: TextButton(
+            onPressed: () {
+              openUrl('https://www.youtube.com/@aajtak');
+            },
+            child: Text('Open URL'),
+          ),
+        ),
+      ),
+    );
+  }
+}
